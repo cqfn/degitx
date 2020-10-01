dc99011e83c300ea0d8f8eba54d0fa447ee991c8
stepanvalyavskiy@gmail.com <Gsetter322>
2020-10-01 16:50:58 +0300

    #4 - go server stub

--- degitx.go
+++ degitx.go
@@ -4,10 +4,43 @@
 package degitx
 
 import (
-	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"org.cqfn/degitx/proto/go/degitxpb"
 )
 
-// Stub - stub function
-func Stub() {
-	fmt.Println("DeGitX stub")
+func Start() {
+	grpcServer := grpc.NewServer()
+
+	degitxpb.RegisterBlobServiceServer(grpcServer, &degitxpb.UnimplementedBlobServiceServer{})
+	degitxpb.RegisterCleanupServiceServer(grpcServer, &degitxpb.UnimplementedCleanupServiceServer{})
+	degitxpb.RegisterCommitServiceServer(grpcServer, &degitxpb.UnimplementedCommitServiceServer{})
+	degitxpb.RegisterDiffServiceServer(grpcServer, &degitxpb.UnimplementedDiffServiceServer{})
+	degitxpb.RegisterNamespaceServiceServer(grpcServer, &degitxpb.UnimplementedNamespaceServiceServer{})
+	degitxpb.RegisterOperationServiceServer(grpcServer, &degitxpb.UnimplementedOperationServiceServer{})
+	degitxpb.RegisterRefServiceServer(grpcServer, &degitxpb.UnimplementedRefServiceServer{})
+	degitxpb.RegisterRepositoryServiceServer(grpcServer, &degitxpb.UnimplementedRepositoryServiceServer{})
+	degitxpb.RegisterSSHServiceServer(grpcServer, &degitxpb.UnimplementedSSHServiceServer{})
+	degitxpb.RegisterSmartHTTPServiceServer(grpcServer, &degitxpb.UnimplementedSmartHTTPServiceServer{})
+	degitxpb.RegisterWikiServiceServer(grpcServer, &degitxpb.UnimplementedWikiServiceServer{})
+	degitxpb.RegisterConflictsServiceServer(grpcServer, &degitxpb.UnimplementedConflictsServiceServer{})
+	degitxpb.RegisterRemoteServiceServer(grpcServer, &degitxpb.UnimplementedRemoteServiceServer{})
+	degitxpb.RegisterServerServiceServer(grpcServer, &degitxpb.UnimplementedServerServiceServer{})
+	degitxpb.RegisterObjectPoolServiceServer(grpcServer, &degitxpb.UnimplementedObjectPoolServiceServer{})
+	degitxpb.RegisterHookServiceServer(grpcServer, &degitxpb.UnimplementedHookServiceServer{})
+	degitxpb.RegisterInternalGitalyServer(grpcServer, &degitxpb.UnimplementedInternalGitalyServer{})
+
+	reflection.Register(grpcServer)
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,9 @@ module org.cqfn/degitx
 
 go 1.14
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/urfave/cli/v2 v2.2.0
+	google.golang.org/grpc v1.27.0
+	google.golang.org/protobuf v1.25.0
+)
