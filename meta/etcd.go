440083ed0b238b487d4133445d096bb7bc7fd7e0
Kirill <g4s8.public@gmail.com>
2020-12-21 18:13:11 +0300

    [#106] - etcd async ops
    Closes #106 - made all etcd `Storage` operations asynchronous,
    passing channel of response to functions and closing channels on
    complete.

--- meta/etcd.go
+++ meta/etcd.go
@@ -15,7 +15,7 @@ import (
 const etcdConTimeout = time.Second * 5
 
 type etcd struct {
-	client clientv3.Client
+	client *clientv3.Client
 }
 
 // NewEtcd creates new Storage, that encapsulates official etcd client
@@ -35,56 +35,61 @@ func NewEtcd(ctx context.Context, endpoints ...string) (Storage, error) {
 		}
 	}()
 
-	return &etcd{*client}, nil
+	return &etcd{client}, nil
 }
 
 // See Storage.Get
-func (storage *etcd) Get(ctx context.Context, key string) <-chan Response {
+func (storage *etcd) Get(ctx context.Context, key string) <-chan *Response {
 	return storage.callEtcd(ctx,
-		func(ctx context.Context, response *Response) {
+		func(ctx context.Context, out chan<- *Response) {
+			rsp := new(Response)
 			get, err := storage.client.Get(ctx, key)
 			if err != nil {
-				response.Error = err
+				rsp.Error = err
 			} else if len(get.Kvs) > 0 {
-				response.Key = string(get.Kvs[0].Key)
-				response.Value = Data(get.Kvs[0].Value)
+				rsp.Key = string(get.Kvs[0].Key)
+				rsp.Value = Data(get.Kvs[0].Value)
 			}
+			select {
+			case out <- rsp:
+			case <-ctx.Done():
+				rsp.Error = ctx.Err()
+				rsp.Value = noData
+			}
+			close(out)
 		})
 }
 
 // See Storage.Set
-func (storage *etcd) Set(ctx context.Context, key string, val Data) <-chan Response {
+func (storage *etcd) Set(ctx context.Context, key string, val Data) <-chan *Response {
 	return storage.callEtcd(ctx,
-		func(ctx context.Context, response *Response) {
+		func(ctx context.Context, out chan<- *Response) {
+			rsp := new(Response)
 			_, err := storage.client.Put(ctx, key, string(val))
 			if err != nil {
-				response.Error = err
+				rsp.Error = err
 			} else {
 				// etcd ensures linearizability for all operations except watch operations
-				response.Key = key
-				response.Value = val
+				rsp.Key = key
+				rsp.Value = val
 			}
+			select {
+			case out <- rsp:
+			case <-ctx.Done():
+				rsp.Error = ctx.Err()
+				rsp.Value = noData
+			}
+			close(out)
 		})
 }
 
 func (storage *etcd) callEtcd(
 	ctx context.Context,
-	query func(ctx context.Context, response *Response),
-) <-chan Response {
+	query func(ctx context.Context, out chan<- *Response),
+) <-chan *Response {
 	ctx, cancel := context.WithTimeout(ctx, etcdConTimeout)
 	defer cancel()
-
-	out := make(chan Response)
-
-	go func() {
-		select { //nolint:gosimple // Done is provided for use in select statements
-		case <-ctx.Done():
-			close(out)
-		}
-	}()
-
-	response := &Response{}
-	query(ctx, response)
-	out <- *response
+	out := make(chan *Response)
+	query(ctx, out)
 	return out
 }
