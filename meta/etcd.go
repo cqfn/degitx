234de4d73a5974b24ff2280fff6727d448160918
Kirill <g4s8.public@gmail.com>
2020-12-24 17:15:43 +0300

    #106 - removed callEtcd func

--- meta/etcd.go
+++ meta/etcd.go
@@ -40,56 +40,51 @@ func NewEtcd(ctx context.Context, endpoints ...string) (Storage, error) {
 
 // See Storage.Get
 func (storage *etcd) Get(ctx context.Context, key string) <-chan *Response {
-	return storage.callEtcd(ctx,
-		func(ctx context.Context, out chan<- *Response) {
-			rsp := new(Response)
-			get, err := storage.client.Get(ctx, key)
-			if err != nil {
-				rsp.Error = err
-			} else if len(get.Kvs) > 0 {
-				rsp.Key = string(get.Kvs[0].Key)
-				rsp.Value = Data(get.Kvs[0].Value)
-			}
-			select {
-			case out <- rsp:
-			case <-ctx.Done():
-				rsp.Error = ctx.Err()
-				rsp.Value = noData
-			}
-			close(out)
-		})
+	ctx, cancel := context.WithTimeout(ctx, etcdConTimeout)
+	defer cancel()
+	out := make(chan *Response)
+	go func() {
+		rsp := new(Response)
+		get, err := storage.client.Get(ctx, key)
+		if err != nil {
+			rsp.Error = err
+		} else if len(get.Kvs) > 0 {
+			rsp.Key = string(get.Kvs[0].Key)
+			rsp.Value = Data(get.Kvs[0].Value)
+		}
+		select {
+		case out <- rsp:
+		case <-ctx.Done():
+			rsp.Error = ctx.Err()
+			rsp.Value = noData
+		}
+		close(out)
+	}()
+	return out
 }
 
 // See Storage.Set
 func (storage *etcd) Set(ctx context.Context, key string, val Data) <-chan *Response {
-	return storage.callEtcd(ctx,
-		func(ctx context.Context, out chan<- *Response) {
-			rsp := new(Response)
-			_, err := storage.client.Put(ctx, key, string(val))
-			if err != nil {
-				rsp.Error = err
-			} else {
-				// etcd ensures linearizability for all operations except watch operations
-				rsp.Key = key
-				rsp.Value = val
-			}
-			select {
-			case out <- rsp:
-			case <-ctx.Done():
-				rsp.Error = ctx.Err()
-				rsp.Value = noData
-			}
-			close(out)
-		})
-}
-
-func (storage *etcd) callEtcd(
-	ctx context.Context,
-	query func(ctx context.Context, out chan<- *Response),
-) <-chan *Response {
 	ctx, cancel := context.WithTimeout(ctx, etcdConTimeout)
 	defer cancel()
 	out := make(chan *Response)
-	query(ctx, out)
+	go func() {
+		rsp := new(Response)
+		_, err := storage.client.Put(ctx, key, string(val))
+		if err != nil {
+			rsp.Error = err
+		} else {
+			// etcd ensures linearizability for all operations except watch operations
+			rsp.Key = key
+			rsp.Value = val
+		}
+		select {
+		case out <- rsp:
+		case <-ctx.Done():
+			rsp.Error = ctx.Err()
+			rsp.Value = noData
+		}
+		close(out)
+	}()
 	return out
 }
