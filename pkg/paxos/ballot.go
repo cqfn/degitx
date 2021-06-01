af74dab7a28862237559b5a04eff9aa8ed8dde9a
Kirill <g4s8.public@gmail.com>
2021-06-01 12:08:00 +0300

    Added Paxos simple abstractions (#191)
    For #177 - added Paxos simple abstractions,
    such as messages, ballots, acceptor API and node,
    proposer API, storage API.

---
+++ pkg/paxos/ballot.go
@@ -0,0 +1,29 @@
+// MIT License. Copyright (c) 2020 CQFN
+// https://github.com/cqfn/degitx/blob/master/LICENSE
+
+package paxos
+
+// Ballot number of proposals in Paxos algorithm
+type Ballot uint16
+
+//nolint:gochecknoglobals // it's a zero constant
+var pZero = Proposal{Ballot: Ballot(0), Proposer: 0}
+
+// Proposal for Paxos protocol
+type Proposal struct {
+	// Ballot number of the proposal
+	Ballot
+	// Proposer ID, it should be unique number for each
+	// proposer participating in the transaction.
+	Proposer uint16
+}
+
+// Compare two proposals
+func (p Proposal) Compare(o Proposal) int {
+	var r int
+	r = int(p.Ballot - o.Ballot)
+	if r == 0 {
+		r = int(p.Proposer - o.Proposer)
+	}
+	return r
+}
